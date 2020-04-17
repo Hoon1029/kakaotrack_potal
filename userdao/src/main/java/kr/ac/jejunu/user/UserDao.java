0e8154568c99c4b80ad50e5c72bae73da179699d
Hoon1029 <hoonisone@gmail.com>
2020-04-17 21:21:15 +0900

    implement insert new user

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -8,11 +8,11 @@ public class UserDao {
         //driver 로딩
         Class.forName("com.mysql.cj.jdbc.Driver");
         //connection
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul",
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/kakao_potal?serverTimezone=Asia/Seoul",
                 "root", "1234");
         //query
         PreparedStatement preparedStatement =
-                connection.prepareStatement("select id, name, password from userinfo where id = ?");
+                connection.prepareStatement("select id, name, password from user_infor where id = ?");
         preparedStatement.setInt(1, id);
         //실행
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -28,5 +28,31 @@ public class UserDao {
         connection.close();
         //결과리턴
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        //mysql
+        //driver 로딩
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //connection
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/kakao_potal?serverTimezone=Asia/Seoul",
+                "root", "1234");
+        //query
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into user_infor (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        //실행
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        //결과매핑
+        user.setId(resultSet.getInt(1));
+        //자원해지
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //결과리턴
     }
 }

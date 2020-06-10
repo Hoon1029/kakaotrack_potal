22f976db4da0a4cff855e450a0a77ff7c86c6024
Hoon1029 <hoonisone@gmail.com>
2020-06-11 08:50:52 +0900

    class week10 - 1 practice: negotiationView and exceptionViow

--- java-framework-class/userdao/src/main/java/kr/ac/jejunu/user/UserController.java
+++ java-framework-class/userdao/src/main/java/kr/ac/jejunu/user/UserController.java
@@ -2,8 +2,10 @@ package kr.ac.jejunu.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
@@ -11,7 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
     private final UserDao userDao;
     @RequestMapping("/user")
-    public User getUser(@RequestParam("id") Integer id) {
-        return userDao.get(id);
+    public User getUser(@RequestParam("id") Integer id) { return userDao.get(id); }
+
+    @RequestMapping("/exception")
+    public void exception(){
+        throw new RuntimeException("어이쿠!");
+    }
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e){
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }

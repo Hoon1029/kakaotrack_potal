f8f4a3ebc477a738b3bf62240c0f72ad8807e8dd
Hoon1029 <hoonisone@gmail.com>
2020-06-21 08:30:08 +0900

    init final projects

--- java-framework-finalexam/coupon_platform/src/main/java/kr/ac/jejunu/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package kr.ac.jejunu;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-}
